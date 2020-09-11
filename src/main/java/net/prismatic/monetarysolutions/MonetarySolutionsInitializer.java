1e04b31620434787f2a9d7c6ed08444da332cd0f
PrismaticYT <realprismaticyt@gmail.com>
2020-09-11 15:21:07 +1200

    Set up development environment

---
+++ src/main/java/net/prismatic/monetarysolutions/MonetarySolutionsInitializer.java
@@ -0,0 +1,10 @@
+package net.prismatic.monetarysolutions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MonetarySolutionsInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
