494828adcccf7ae4b88ba6846de487af08742912
PrismaticYT <realprismaticyt@gmail.com>
2020-09-11 17:39:03 +1200

    Why didn't it commit properly?

--- src/main/java/net/prismatic/monetarysolutions/components/PlayerMoneyComponent.java
+++ src/main/java/net/prismatic/monetarysolutions/components/PlayerMoneyComponent.java
@@ -1,2 +1,48 @@
-package net.prismatic.monetarysolutions.components;public class PlayerMoneyComponent {
+package net.prismatic.monetarysolutions.components;
+
+import dev.onyxstudios.cca.api.v3.entity.PlayerComponent;
+import nerdhub.cardinal.components.api.ComponentType;
+import net.minecraft.nbt.CompoundTag;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+
+public class PlayerMoneyComponent implements PlayerComponent {
+    private BigDecimal money;
+
+    public PlayerMoneyComponent() {
+        this.money = new BigDecimal("0.00");
+    }
+
+    @Override
+    public ComponentType<?> getComponentType() {
+        return null;
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        this.money = new BigDecimal(tag.getString("money"));
+    }
+    @Override
+    public @NotNull CompoundTag toTag(CompoundTag tag) {
+        tag.putString("money", this.money.toString());
+        return tag;
+    }
+
+    public void spend(String amount) {
+        BigDecimal subtractor = this.money.subtract(new BigDecimal(amount));
+        if (subtractor.signum() == -1) {
+            return;
+        } else {
+            this.money = subtractor;
+        }
+    }
+
+    public void pay(String amount) {
+        this.money = this.money.add(new BigDecimal(amount));
+    }
+
+    public String amount() {
+        return this.money.toString();
+    }
 }
